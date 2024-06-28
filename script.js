e481974b9a6599ac72839c72c3de275507700d45
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-06-28 15:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const output = document.getElementById('output');
+const arr = [1, 2, 3, 4];
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {resolve(arr)}, 1000);
+});
+
+promise
+    .then((a) => {
+        const A = a.filter((n) => (n % 2 == 0));
+        const output1 = document.createElement('p');
+        output1.innerHTML = `The filtered array is: [${A.join(', ')}]`;
+        output.appendChild(output1);
+
+        return A;
+    })
+    .then((b) => {
+        setTimeout(() => {
+            const B = b.map((n) => (n * 2));
+            const output2 = document.createElement('p');
+            output2.innerHTML = `The squared array is: [${B.join(', ')}]`;
+            output.appendChild(output2);
+        }, 2000);
+    });
